11cfdec7f5cb7e47f28dfe0aa6b64b60eaa1bd9b
bem630 <bah70495@gmail.com>
2024-02-21 17:45:56 +0100

    ✨ F01: Add tile and form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useState } from "react";
+
 function App() {
+  const [inputValue, setInputValue] = useState("")
+
+  //comportements
+  const handleSubmit = (event) => { 
+    alert(`Bonjour ${inputValue}`)
+    setInputValue("");
+   }
+
+   const handleChange = (event) => { 
+    setInputValue(event.target.value)
+    }
+
+  //Affichage
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+      <form action="submit" onSubmit={handleSubmit}>
+        <h1>bienvenue chez nous !</h1>
+        <h1>Connectez vous</h1>
+        <input 
+          value={inputValue} 
+          onChange={handleChange} 
+          type="text" 
+          placeholder="Entrez votre prénom..." 
+          required />
+        <button>Accéder à votre espace</button>
+      </form>
   );
 }
 
