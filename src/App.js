6227582dbedbb9a39a944e975c99dbde13afba5b
bem630 <bah70495@gmail.com>
2024-02-21 18:03:31 +0100

    ♻️ F01: Add LoginForm and Logo into LoginPage

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import { useState } from "react";
+import LoginPage from "./components/pages/login/LoginPage";
 
 function App() {
-  const [inputValue, setInputValue] = useState("")
-
-  //comportements
-  const handleSubmit = (event) => { 
-    alert(`Bonjour ${inputValue}`)
-    setInputValue("");
-   }
-
-   const handleChange = (event) => { 
-    setInputValue(event.target.value)
-    }
-
-  //Affichage
+  
 
   return (
-      <form action="submit" onSubmit={handleSubmit}>
-        <h1>bienvenue chez nous !</h1>
-        <h1>Connectez vous</h1>
-        <input 
-          value={inputValue} 
-          onChange={handleChange} 
-          type="text" 
-          placeholder="Entrez votre prénom..." 
-          required />
-        <button>Accéder à votre espace</button>
-      </form>
+      <div>
+        <LoginPage/>
+      </div>
   );
 }
 
