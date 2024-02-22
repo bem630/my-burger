39647e464c5b88ab2a80a9398f27059fae91b03e
bem630 <bah70495@gmail.com>
2024-02-22 20:10:32 +0100

    ✨ F03:  Create Profile and add icon

---
+++ src/components/pages/order/navbar/NavbarRightSide.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+import Profile from './Profile';
+
+export default function NavbarRightSide() {
+    
+  return (
+    <NavbarRightSideStyled>
+        <Profile />
+    </NavbarRightSideStyled>
+  )
+}
+
+const NavbarRightSideStyled = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 50px;
+`
