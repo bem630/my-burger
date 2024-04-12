9e3c893c0935f6e00141dc8890a73ef5cdf957eb
bem630 <bah70495@gmail.com>
2024-04-12 11:29:53 +0100

    ✨ F05: Admin > Add component

---
+++ src/components/pages/order/main/MainRightSide/Admin/AdminPanel/AdminPanel.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import styled from 'styled-components';
+
+import { getTabSelected, tabsConfig } from '../tabsConfig';
+import OrderContext from '../../../../../../../context/OrderContext';
+import { theme } from '../../../../../../../theme';
+
+
+export default function AdminPanel() {
+  const {currentTabSelected} = useContext(OrderContext)
+
+  const tabs = tabsConfig(currentTabSelected)
+
+  const tabSelected = getTabSelected(tabs, currentTabSelected)
+
+  return (
+    <AdminPanelStyled>
+      {/*currentTabSelected === "add" ? "Ajouter un produit": "Modifier un produit"*/}
+      { tabSelected && tabSelected.Content}
+    </AdminPanelStyled>
+  )
+}
+
+const AdminPanelStyled = styled.div`
+  height: 240px;
+  background: ${theme.colors.white};
+  border: 1px solid ${theme.colors.greyLight};
+  box-shadow: ${theme.shadows.subtle};
+  box-sizing: border-box; // ajouter ça sinon ça fait grossir le panel
+  padding: 30px 5%; // ajouter au panel et non dans les form car sinon on va devoir le mettre dans les deux form : AddForm et EditForm
+  // les 5% c'est pour les aligner avec le tab.
+`
